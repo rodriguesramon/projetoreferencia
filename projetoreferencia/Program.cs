e68615143a6810e96daffcfee7b7e5fac4a6711c
Ramon Costa <ramon.rodrigues.lima@gmail.com>
2018-11-05 23:28:40 -0400

    Commit Inicial Projeto com Passagem de Valores

---
+++ projetoreferencia/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace projetoreferencia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Valor valor = new Valor();
+
+            int n = 5;
+            valor.passagemPorValor(n);
+            Console.WriteLine("Por valor: " + n);
+
+            valor.passagemPorReferencia(ref n);
+            Console.WriteLine("Por referencia: " + n);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ projetoreferencia/Valor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace projetoreferencia
+{
+    public class Valor
+    {
+        public void passagemPorValor(int a){
+            a = 10;
+        }
+
+        public void passagemPorReferencia(ref int a){
+            a = 10;
+        }
+    }
+}
